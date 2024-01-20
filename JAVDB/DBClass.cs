db5e17566c8c076436cbcb6a25aba538da4a301d
友 竭 <jieyou1006@hotmail.com>
2024-01-20 22:32:18 +0800

    First Check in

---
+++ JAVDB/DBClass.cs
@@ -0,0 +1,106 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GETMOOTOOL
+{ 
+    class DBClass
+    {
+        private string strConn = "server=172.16.1.2; Port=3306; user id=jy2029; password=123456; database=db; pooling=false; charset=utf8";
+        public string strerr = "";
+        /// <summary>
+        /// 执行SQL返回DataTable
+        /// </summary>
+        /// <param name="strSql"></param>
+        /// <returns></returns>
+        public DataTable ReturnDataTable(string strSql)
+        {
+            DataTable dt = new DataTable();
+            MySqlConnection con = new MySqlConnection(strConn);
+            try
+            {
+                con.Open();
+                MySqlCommand cmd = con.CreateCommand();
+                cmd.CommandText = strSql;
+                MySqlDataAdapter adap = new MySqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adap.Fill(ds);
+                dt = ds.Tables[0];
+            }
+            catch(Exception ex)
+            {
+                strerr = ex.ToString();
+                con.Close();
+                con.Dispose();
+            }
+            finally
+            {
+                con.Close();
+                con.Dispose();
+            }
+
+            return dt;
+        }
+
+        /// <summary>
+        /// 执行SQL
+        /// </summary>
+        /// <param name="strSql"></param>
+        /// <returns></returns>
+        public string ReturnString(string strSql)
+        {
+            string strResult = "OK";
+            MySqlConnection con = new MySqlConnection(strConn);
+            try
+            {
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand(strSql, con);
+                cmd.ExecuteNonQuery();
+            }
+            catch(Exception ex)
+            {
+                strResult = ex.ToString();
+                con.Close();
+                con.Dispose();
+            }
+            finally
+            {
+                con.Close();
+                con.Dispose();
+            }
+            return strResult;
+        }
+
+        /// <summary>
+        /// 执行SQL不管执行结果
+        /// </summary>
+        /// <param name="strSql"></param>
+        public void NonResturn(string strSql)
+        {
+            MySqlConnection con = new MySqlConnection(strConn);
+            try
+            {
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand(strSql, con);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                strerr = ex.ToString();
+                con.Close();
+                con.Dispose();
+            }
+            finally
+            {
+                con.Close();
+                con.Dispose();
+            }
+        }
+
+
+    }
+}
---
+++ JAVDB/Movie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GETMOOTOOL
+{
+    class Movie
+    {
+        public string Code = "";         //影片代码
+        public string MovieName = "";    //名字
+        public string PublishTime = "";  //发行时间
+        public string Times = "";        //时长
+        public string Director = "";     //导演
+        public string Maker = "";        //制作商
+        public string Publisher = "";    //发行商
+        public string Series = "";       //系列
+        public string ImgUrl = "";       //封面图url 
+        public string SmallImgUrl = "";  //缩小的封面url
+        public ArrayList ListType = new ArrayList(); //类型
+        public ArrayList ListActerName = new ArrayList();  //演员
+        public ArrayList ListActerIndexUrl = new ArrayList(); //演员主页url
+        public ArrayList ListActerImgUrl = new ArrayList();  //演员头像url
+        public ArrayList ListSnapshotUrl = new ArrayList();  //影片快照url
+        public ArrayList ListSmallSnapshotUrl = new ArrayList();  //缩小的影片快照url
+        public byte[] bImg;
+        public byte[] bSmallImg;
+        public List<byte[]> bShotImg = new List<byte[]>();
+        public List<byte[]> bSmallShotImg = new List<byte[]>();
+    }
+
+}
